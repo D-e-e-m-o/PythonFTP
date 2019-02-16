5cf3ee4ba823908c8f0b8a4521afaef549c8a6d8
deemoding <wsd009811@gmail.com>
2019-02-16 18:47:53 +0800

    移除.idea；修改.gitignore；微调ftp.py

--- ftp.py
+++ ftp.py
@@ -5,6 +5,7 @@ from pyftpdlib.servers import FTPServer
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 
+
 def main():
     authorizer = DummyAuthorizer()
     authorizer.add_user('ADING', '123456', '.', perm='elrwm')
@@ -13,12 +14,13 @@ def main():
     handler = FTPHandler
     handler.authorizer = authorizer
     # requires SSL for both control and data channel
-    #handler.tls_control_required = True
-    #handler.tls_data_required = True
-    address = ('',4000)
+    # handler.tls_control_required = True
+    # handler.tls_data_required = True
+    address = ('', 4000)
     logging.basicConfig(filename='./pyftpd.log',level=logging.INFO)
     server = FTPServer(address, handler)
     server.serve_forever()
+
 
 if __name__ == '__main__':
     main()
